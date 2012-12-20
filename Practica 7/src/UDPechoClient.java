4e1adaae587744fe2394eb283da9c1e08db2f2d2
efro89 <efro89@hotmail.com>
2012-12-20 17:43:19 +0100

    Update Practica 7/src/UDPechoClient.java
    esta modificacion es para que el programa cliente funcione con dos hebras

--- Practica 7/src/UDPechoClient.java
+++ Practica 7/src/UDPechoClient.java
@@ -7,7 +7,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Date;
 
-public class UDPechoClient {
+public class cliente72 {
 	public static void main (String args[]) throws Exception {
 		// consigue los parametros
 		String host = "localhost";
@@ -15,14 +15,14 @@ public class UDPechoClient {
 		InetAddress address = InetAddress.getByName(host);
 		int serverPort = 1255;
 		if(args.length>1) serverPort = Integer.parseInt(args[1]);
-
+		DatagramSocket Socket = new DatagramSocket();
 		// itera en un bucle infinito lanzando hebras de echo
 		// hasta que se escribe un punto "." para salir
 		boolean iterar = true;
 		while (iterar) {
 			String linea = getTeclado();
-			new EnviaThread(address, serverPort, linea).start();
-			new RecibeThread(address, serverPort).start();
+			new hiloenviar(address, serverPort, linea,Socket).start();
+			new hilorecivir(address, serverPort, linea,Socket).start();
 			if(linea.equals(".")) iterar = false;
 		} 
 	}
@@ -42,87 +42,30 @@ public class UDPechoClient {
 	}
 }
 
-
-
-
 /*
  * Realiza la operación de "echo". Con los parámetros que se le
  *  pasa intenta enviar y recibir una cadea.
  */
-class EnviaThread extends Thread{
+class hilorecivir extends Thread{
 	private InetAddress address;
 	private int port;
 	private DatagramSocket ds;
 	private String mensaje;
-	public EnviaThread(InetAddress address, int port, String mensaje) {
+	
+	public hilorecivir(InetAddress address, int port, String mensaje, DatagramSocket s2) {
 		super();
 		this.address	= address;
 		this.port 		= port;
 		this.mensaje	= mensaje;
-		try {
-			this.ds  	= new DatagramSocket();
-		} catch (SocketException e) { e.printStackTrace(); }
+		//try {
+			this.ds  	= s2;
+		//} catch (SocketException e) { e.printStackTrace(); }
 	}
 
 	@Override
 	public void run() {
-		enviaString();
-		super.run();
-	}
-
-	/**
-	 * Envia una cadena de texto.
-	 */
-	private void enviaString() {
-		byte[] data = this.mensaje.getBytes();
-		System.out.println();
-		System.out.println(" enviado en bytes	:" + data);
-		System.out.println(" enviado en string	:" + mensaje);
 		
-		DatagramPacket output = new DatagramPacket(data, data.length, address, port);
-		try {
-			ds.send(output);
-		} catch (IOException e) {e.printStackTrace();}
-	}
-		/**
-		 * Recibe una cadena de texto.
-		 */
-		private void recibeString() {
-			byte[] buffer = new byte[1024];
-			DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
-			try {
-				ds.receive(dp);
-				System.out.println(" recibido en bytes	:" + dp.getData());
-				System.out.println(" recibido en string	:" + new String(dp.getData()));
-			} catch (IOException ex) { 	System.err.println(ex);
-			} finally {
-				ds.close();
-			}
-	}
-}
-
-/*
- * Realiza la operación de "echo". Con los parámetros que se le
- *  pasa intenta enviar y recibir una cadea.
- */
-class RecibeThread extends Thread{
-	private InetAddress address;
-	private int port;
-	private DatagramSocket ds;
-	public RecibeThread(InetAddress address, int port) {
-		super();
-		this.address	= address;
-		this.port 		= port;
-		try {
-			this.ds  	= new DatagramSocket();
-		} catch (SocketException e) { e.printStackTrace(); }
-	}
-
-	@Override
-	public void run() {
-		System.out.println("recibiendo");
 		recibeString();
-		System.out.println("recibido");
 		super.run();
 	}
 
@@ -135,10 +78,52 @@ class RecibeThread extends Thread{
 		try {
 			ds.receive(dp);
 			System.out.println(" recibido en bytes	:" + dp.getData());
-			System.out.println(" recibido en string	:" + new String(dp.getData()));
+			System.out.println(" recibido en string	:" + new String(dp.getData(), 0, dp.getLength()));
 		} catch (IOException ex) { 	System.err.println(ex);
-		} finally {
-			ds.close();
-		}
+		} //finally {
+			//ds.close();
+		//}
+	}
+}
+
+/*
+ * Realiza la operación de "echo". Con los parámetros que se le
+ *  pasa intenta enviar y recibir una cadea.
+ */
+class hiloenviar extends Thread{
+	private InetAddress address;
+	private int port;
+	private DatagramSocket ds;
+	private String mensaje;
+	public hiloenviar(InetAddress address, int port, String mensaje,DatagramSocket s2) {
+		super();
+		this.address	= address;
+		this.port 		= port;
+		this.mensaje	= mensaje;
+		//try {
+			this.ds  	= s2;
+		//} catch (SocketException e) { e.printStackTrace(); }
+	}
+
+	@Override
+	public void run() {
+		enviaString();
+		
+		super.run();
+	}
+
+	/**
+	 * Envia una cadena de texto.
+	 */
+	private void enviaString() {
+		byte[] data = this.mensaje.getBytes();
+		System.out.println();
+		System.out.println(" enviado en bytes	:" + data);
+		System.out.println(" enviado en string	:" + mensaje);
+
+		DatagramPacket output = new DatagramPacket(data, data.length, address, port);
+		try {
+			ds.send(output);
+		} catch (IOException e) {e.printStackTrace();}
 	}
 }
